a777627fb5c3edc6fa4f63086da66801c5489f86
Daniel <tacombel@gmail.com>
2022-02-17 13:31:16 +0100

    Reorganizo codigo

--- siguiente_letra.py
+++ siguiente_letra.py
@@ -2,31 +2,39 @@ import csv
 from pickle import TRUE
 from time import time
 
-listado_de_palabras = []
-with open('listado_de_palabras_sin_tildes.txt', 'r') as fd:
-    reader = csv.reader(fd)
-    for row in reader:
-        listado_de_palabras.append(row[0])
+def cargar_datos():
+    listado_de_palabras = []
+    with open('listado_de_palabras_sin_tildes.txt', 'r') as fd:
+        reader = csv.reader(fd)
+        for row in reader:
+            listado_de_palabras.append(row[0])
+    return listado_de_palabras
 
-x = input('Introduce las letras: ')
-print()
-inicio = time()
-y = len(x)
-palabras_coincidentes = 0
-candidatas = []
-match = False
-for e in listado_de_palabras:
-    if len(e) <= y:
-        continue
-    elif e.startswith(x):
-        palabras_coincidentes += 1
-        match = True
-        if e[y] not in candidatas:
-            candidatas.append(e[y])
-    elif match == True and e.startswith(x) == False:
-        print(e)
-        break
-print(candidatas)
-print(f'Palabras coincidentes: {palabras_coincidentes}')
-print("--- %s seconds ---" % (time() - inicio))
-print()
+def busqueda_de_letras(data):
+    x = input('Introduce las letras: ')
+    print()
+    inicio = time()
+    y = len(x)
+    palabras_coincidentes = 0
+    candidatas = []
+    match = False
+    for e in data:
+        if len(e) <= y:
+            continue
+        elif e.startswith(x):
+            palabras_coincidentes += 1
+            match = True
+            if e[y] not in candidatas:
+                candidatas.append(e[y])
+        elif match == True and e.startswith(x) == False:
+            break
+    print(f'Palabras coincidentes: {palabras_coincidentes}')
+    print("--- %s seconds ---" % (time() - inicio))
+    return candidatas
+
+def main():
+    print (busqueda_de_letras(cargar_datos()))
+
+
+if __name__ == "__main__":
+    main()
