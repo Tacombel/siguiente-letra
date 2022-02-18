6f5baa624571dac383261c64e36a006bc4fd5821
Daniel <tacombel@gmail.com>
2022-02-18 11:37:46 +0100

    Creo un nuevo listado de palabras que conserva las tildes

--- quitar_tildes.py
+++ quitar_tildes.py
@@ -1,5 +1,6 @@
 import csv
-import unidecode
+import re
+from unicodedata import normalize
 
 listado_de_palabras = []
 with open('0_palabras_todas.txt', 'r') as fd:
@@ -9,8 +10,11 @@ with open('0_palabras_todas.txt', 'r') as fd:
 
 listado_de_palabras_sin_tildes = []
 for e in listado_de_palabras:
-    sin_tildes = unidecode.unidecode(e)
-    listado_de_palabras_sin_tildes.append(sin_tildes)
+    s = re.sub(
+        r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1", 
+        normalize( "NFD", e), 0, re.I
+    )
+    listado_de_palabras_sin_tildes.append(s)
 
 with open('listado_de_palabras_sin_tildes.txt', 'w') as file:
     for item in listado_de_palabras_sin_tildes:
