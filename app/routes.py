54d2b79b5b8f946e726e5a41236c1c5beb0cd971
Daniel <tacombel@gmail.com>
2022-02-20 00:15:05 +0100

    Empiezo a darle la forma del prototipo

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, render_template
+from flask import flash, redirect, render_template, session, url_for
 
 from app import app
 from app.forms import palabra_en_formacion
@@ -6,14 +6,18 @@ import siguiente_letra
 
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = palabra_en_formacion()
     candidatas = []
+    if not session['letras']:
+        session['letras'] = ""
     if form.validate_on_submit():
         letras = form.letras.data
         letras = letras.lower()
+        session['letras'] = letras
         candidatas = siguiente_letra.main(letras)
-        flash('Has introducido las siguientes letras: {}'.format(letras))
+        session['candidatas'] = candidatas
         flash('Candidatas para la siguiente letra {}'.format(candidatas))
-        return redirect('/')
-    return render_template('index.html', form=form)
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, letras=session['letras'])
