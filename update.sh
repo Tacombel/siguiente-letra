f2e9284a18906d274a07b734fffe06776a79133b
Daniel <tacombel@gmail.com>
2022-02-19 12:20:19 +0100

    Corrijo el script

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo systemctl stop siguiente-letra &&
 git pull origin master &&
-/.venv/bin/pip -r requeriments.txt &&
+source ./venv/bin/activate &&
+pip -r requeriments.txt &&
 sudo systemctl start siguiente-letra
