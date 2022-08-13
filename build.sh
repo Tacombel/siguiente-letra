9080a37a216b692e976dc9793507af8b8b43bb5d
Daniel <tacombel@gmail.com>
2022-08-13 09:50:10 +0200

    build.sh

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build -t tacombel/siguiente-letra:v1.0 .
+docker push tacombel/siguiente-letra:v1.0
--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo systemctl stop siguiente-letra &&
-git pull origin master &&
-source /home/ubuntu/www/siguiente-letra/.venv/bin/activate &&
-pip install -r requeriments.txt &&
-sudo systemctl start siguiente-letra
