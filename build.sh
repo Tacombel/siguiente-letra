d6d228ea76394f56f614780cc4fc6966b20f25c2
Daniel <tacombel@gmail.com>
2022-08-14 12:28:53 +0200

    Updated version

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t tacombel/siguiente-letra:v1.0 .
-docker push tacombel/siguiente-letra:v1.0
+docker build -t tacombel/siguiente-letra:v1.0.1 .
+docker push tacombel/siguiente-letra:v1.0.1
